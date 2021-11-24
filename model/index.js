536e60833dfcfc725bc8d9e9a7aa63ef0bfdcba0
Huy Doan <huy11900@gmail.com>
2021-11-24 16:32:44 +0700

    Host dtb info

--- model/index.js
+++ model/index.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require('sequelize');
 const initModels = require('./init-models');
-const sequelize = new Sequelize(process.env.DB_DTB, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(process.env.DB_URL);
 
 module.exports = {
     sequelize,
