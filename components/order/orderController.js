8e51b38fd14b73afc4f766956de46f9cec5e2e65
Huy Doan <huy11900@gmail.com>
2021-12-21 18:57:48 +0700

    Checkout page

--- components/order/orderController.js
+++ components/order/orderController.js
@@ -1,5 +1,36 @@
-const checkout = (req, res, next) => {
-    res.render('order/checkout', { title: 'Checkout', style: 'checkout.css' });
+const cartService = require('../cart/cartService');
+const profileService = require('../profile/profileService');
+
+const checkout = async (req, res, next) => {
+    const user = req.user;
+    if (user) {
+        try {
+            const [product, address] = await Promise.all([ 
+                cartService.getCartById(user.id),
+                profileService.getAddresses(user.id)
+            ]);
+            const firstAddress = address[0];
+            const otherAddress = address.slice(1);
+            const total = product.reduce((prev, cur) => {
+                return cur.total + prev;
+            }, 0);
+            const totalWithShip = total + 15000;
+            res.render('order/checkout', {
+                title: 'Checkout',
+                style: 'checkout.css',
+                scripts: ['cart.js'],
+                firstAddress,
+                otherAddress,
+                product,
+                total,
+                totalWithShip
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.redirect('/auth/login')
+    }
 }
 
 const confirm = (req, res, next) => {
